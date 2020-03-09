fc8f84622d932ae9c39754ed2dc9c23d06d9ab49
Amnzeez <ameen.aziz@mccann.com>
2020-03-09 16:55:48 +0000

    using react create-react-app and adding basic components header, main and footer

---
+++ src/components/Header.jsx
@@ -0,0 +1,7 @@
+import React, { Components } from "react";
+
+function Header() {
+  return <header>{/* <nav> links yo</nav> */}</header>;
+}
+
+export default Header;
---
+++ src/components/Main.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Main() {
+  return (
+    <main>
+      <div className="Aligner">
+        <div className="Aligner-item">
+          <h1>Igniteworx</h1>
+          <h2>website development</h2>
+          <h3>hello@igniteworx.com</h3>
+        </div>
+      </div>
+    </main>
+  );
+}
+
+export default Main;
