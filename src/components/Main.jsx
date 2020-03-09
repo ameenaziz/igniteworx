12b3d400e25fe43d506052410e9a672b3ee3ecc3
Amnzeez <ameen.aziz@mccann.com>
2020-03-09 17:01:19 +0000

    tagline update

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -6,7 +6,7 @@ function Main() {
       <div className="Aligner">
         <div className="Aligner-item">
           <h1>Igniteworx</h1>
-          <h2>website development</h2>
+          <h2>Web and mobile apps </h2>
           <h3>hello@igniteworx.com</h3>
         </div>
       </div>
