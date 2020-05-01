87e0c40f4e1ba36af10af1513854521c36d37a50
Amnzeez <ameen.aziz@mccann.com>
2020-05-01 03:29:16 +0100

    remvoing

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,16 +2,10 @@ import React from 'react'
 import Link from 'gatsby-link'
 
 const IndexPage = () => (
-
-
-<section className="attactorSection">
-
-  <h1>Web and mobile apps</h1>
-  <p>Fast and Flexible</p>
-
-</section>
-
-  
+  <section className="attactorSection">
+    <h1>Web and mobile apps</h1>
+    <p>Fast and Flexible</p>
+  </section>
 )
 
 export default IndexPage
